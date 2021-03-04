80494c174f4c7bd88738e75a3289ad751d5ae8ff
Rodrigo Gamboa Loera <RodrigoGamboaLoera@hotmail.com>
2021-03-04 12:30:41 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Alumnos
+{
+    class Persona
+    {
+        static void Main(string[] args)
+        {
+            var nombre = "Pikachu"
+            var apellido = "Gonzalez"
+
+            Console.WriteLine("hola! + nombre + apellido + "es mi nombre completo");
+        }
+    }
+}
